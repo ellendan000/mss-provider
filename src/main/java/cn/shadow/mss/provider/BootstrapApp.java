366711b6d816bd175ce280963d8170bbcbe990fb
rldan <rldan@thoughtworks.com>
2019-03-31 03:28:37 +0800

    add spring boot strap

---
+++ src/main/java/cn/shadow/mss/provider/BootstrapApp.java
@@ -0,0 +1,12 @@
+package cn.shadow.mss.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class BootstrapApp {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(BootstrapApp.class);
+        application.run(args);;
+    }
+}
