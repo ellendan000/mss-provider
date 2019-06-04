cab8d961047d0f2254a37bde6bee9c06e4b04704
rldan <rldan@thoughtworks.com>
2019-06-04 15:27:14 +0800

    enable health check

---
+++ src/main/java/cn/shadow/mss/provider/configuration/CustomizeWebSecurityConfigure.java
@@ -0,0 +1,19 @@
+package cn.shadow.mss.provider.configuration;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+
+@EnableWebSecurity
+public class CustomizeWebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().disable();
+        http.securityContext().disable();
+        http.requestCache().requestCache(new NullRequestCache());
+        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
+                .anyRequest().permitAll();
+    }
+}
