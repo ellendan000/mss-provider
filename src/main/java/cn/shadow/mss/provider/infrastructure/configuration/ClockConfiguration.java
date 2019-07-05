d60f1b5afcd6a1fafa183e56c17b8ffb11778476
rldan <rldan@thoughtworks.com>
2019-07-05 18:55:39 +0800

    add the utc clock configuration

---
+++ src/main/java/cn/shadow/mss/provider/infrastructure/configuration/ClockConfiguration.java
@@ -0,0 +1,14 @@
+package cn.shadow.mss.provider.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class ClockConfiguration {
+    @Bean
+    public Clock globalClock() {
+        return Clock.systemUTC();
+    }
+}
