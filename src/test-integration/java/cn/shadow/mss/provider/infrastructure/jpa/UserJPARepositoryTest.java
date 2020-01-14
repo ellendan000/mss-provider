7c6ee2ddddc04058cb2533c074b6f79daf78cbd1
rldan <rldan@thoughtworks.com>
2020-01-14 17:04:57 +0800

    tests: add the assertion

--- src/test-integration/java/cn/shadow/mss/provider/infrastructure/jpa/UserJPARepositoryTest.java
+++ src/test-integration/java/cn/shadow/mss/provider/infrastructure/jpa/UserJPARepositoryTest.java
@@ -6,12 +6,14 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserJPARepositoryTest extends JPABaseTest {
 
-//    @Autowired
-//    private TestEntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Autowired
     private UserJPARepository userJPARepository;
@@ -33,6 +35,9 @@ class UserJPARepositoryTest extends JPABaseTest {
                 .accounts(Lists.newArrayList(givenAccountPO))
                 .build();
         userJPARepository.save(givenUserPO);
+
+        UserPO userPO = entityManager.find(UserPO.class, givenUserPO.getOid());
+        assertEquals(1, userPO.getAccounts().size());
     }
 
 }
